ede3f89106ad795b63a5a139e5bbd11b3d50f7f3
Syo Ikeda <suicaicoca@gmail.com>
2015-10-15 16:35:19 +0900

    go fmt

--- parser/parser.go
+++ parser/parser.go
@@ -1,61 +1,60 @@
 package parser
+
 import (
-    "regexp"
-    "log"
-    "fmt"
-    "strings"
-    "net/http"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
-
-
 func Fetch(line string) (match bool, key string, license string) {
-    if match, k, url := Parse(line); match {
-        l := get(url)
-        if l == "" {
-            return false, k, ""
-        }
-        l = strings.Replace(l, "<", "", -1)
-        l = strings.Replace(l, ">", "", -1)
-        return true, k, l
-    }
-    return false, "", ""
+	if match, k, url := Parse(line); match {
+		l := get(url)
+		if l == "" {
+			return false, k, ""
+		}
+		l = strings.Replace(l, "<", "", -1)
+		l = strings.Replace(l, ">", "", -1)
+		return true, k, l
+	}
+	return false, "", ""
 }
 
 func Parse(line string) (match bool, key string, url string) {
-    log.Println(line)
+	log.Println(line)
 
-    r, _ := regexp.Compile(`\[[\-\.\w]+\]:`)
+	r, _ := regexp.Compile(`\[[\-\.\w]+\]:`)
 
-    lib := r.FindString(line)
-    fmt.Println(lib)
-    if len(lib) > 0 {
-        u := strings.Replace(line, lib, "", 1)
-        lib = strings.Replace(lib, "[", "", 1)
-        lib = strings.Replace(lib, "]:", "", 1)
-        log.Printf("%s: %s", lib, u)
+	lib := r.FindString(line)
+	fmt.Println(lib)
+	if len(lib) > 0 {
+		u := strings.Replace(line, lib, "", 1)
+		lib = strings.Replace(lib, "[", "", 1)
+		lib = strings.Replace(lib, "]:", "", 1)
+		log.Printf("%s: %s", lib, u)
 
-        u = strings.Replace(u, "https://github.com/", "https://raw.githubusercontent.com/", 1)
-        u = strings.Replace(u, "/blob/master/", "/master/", 1)
-        u = strings.Replace(u, "/blob/develop/", "/develop/", 1)
+		u = strings.Replace(u, "https://github.com/", "https://raw.githubusercontent.com/", 1)
+		u = strings.Replace(u, "/blob/master/", "/master/", 1)
+		u = strings.Replace(u, "/blob/develop/", "/develop/", 1)
 
-        return true, lib, u
-    }
-    return false, "", ""
+		return true, lib, u
+	}
+	return false, "", ""
 }
 
 func get(url string) string {
-    log.Println("url: " + url)
-    r, e := http.Get(url)
-    if e != nil {
-        return ""
-    }
-    defer r.Body.Close()
+	log.Println("url: " + url)
+	r, e := http.Get(url)
+	if e != nil {
+		return ""
+	}
+	defer r.Body.Close()
 
-    bs, e := ioutil.ReadAll(r.Body)
-    if e != nil {
-        return ""
-    }
-    return string(bs)
+	bs, e := ioutil.ReadAll(r.Body)
+	if e != nil {
+		return ""
+	}
+	return string(bs)
 }
